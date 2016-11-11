f20428db2c0bf568b5168ea6c1ff5076979483e2
Aaron1515 <aaron1515@gmail.com>
2016-11-11 11:07:30 -0800

    1. Pseudocode
    2. Translate pseudocode

--- exercise.rb
+++ exercise.rb
@@ -5,6 +5,29 @@ class Exercise
   # If the word being replaced has a capital first letter, it should instead be replaced with "Marklar".
   def self.marklar(str)
     # TODO: Implement this method
+
+    # Create an empty string
+    new_str = Array.new
+
+    # Take the string and seperate each word.
+        # Check each word
+    # IF - over 4 chars long and is capitalize, replace with Markar
+    # If - just over 4 char long replace with markar.
+    # ELSE - Put the word in the string
+    # end
+    str.split(' ').each do |word|
+      if word.length > 4 
+        if word == word.capitalize
+          new_str << word.gsub(/(\w+)/, "Marklar")
+        else
+          new_str << word.gsub(/(\w+)/, "marklar")
+        end
+      else
+        new_str << word
+      end
+    end
+
+    new_str.join(' ')
   end
 
   # Return the sum of all even numbers in the Fibonacci sequence, up to
@@ -13,6 +36,22 @@ class Exercise
   # and the sum of its even numbers is (2 + 8) = 10
   def self.even_fibonacci(nth)
     # TODO: Implement this method
+
+    # Return 0 unless number is higher than 2
+    return 0 unless nth > 2
+
+    # Init fibonacci sequence
+    fib_sequence = [1, 1]
+
+    # While INPUT number is larger than fib_sequence
+      # ADD to the end of fib_sequence
+    while fib_sequence.size < nth
+      fib_sequence << fib_sequence[-1] + fib_sequence[-2]
+      nth - 1
+    end
+
+    # Selet only even numbers, then add them together
+    fib_sequence.select{ |i| i.even?}.inject(:+)
   end
 
 end
